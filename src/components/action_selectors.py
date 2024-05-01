e0627a3239a642507fdc104615d6756653a9fc9c
Josh Holder <josh.holder72@gmail.com>
2024-05-01 14:46:38 -0700

    add epsilon_greedy/sap test

--- src/components/action_selectors.py
+++ src/components/action_selectors.py
@@ -65,6 +65,48 @@ class EpsilonGreedyActionSelector():
 
 REGISTRY["epsilon_greedy"] = EpsilonGreedyActionSelector
 
+class EpsilonGreedySAPTestActionSelector():
+    """
+    Epsilon greedy for training, but constrained to assignment problems for testing.
+    """
+    def __init__(self, args):
+        self.args = args
+
+        self.schedule = DecayThenFlatSchedule(args.epsilon_start, args.epsilon_finish, args.epsilon_anneal_time,
+                                              decay="linear")
+        self.epsilon = self.schedule.eval(0)
+
+    def select_action(self, agent_inputs, avail_actions, t_env, test_mode=False):
+        # Assuming agent_inputs is a batch of Q-Values for each agent bav
+        self.epsilon = self.schedule.eval(t_env)
+
+        num_batches = agent_inputs.shape[0]
+        n_agents = agent_inputs.shape[1]
+        n_actions = agent_inputs.shape[2]
+
+        if test_mode:
+            picked_actions = th.zeros(num_batches, n_agents)
+            for batch in range(num_batches):
+                # Solve the assignment problem for each batch, converting to numpy first
+                benefit_matrix_from_q_values = agent_inputs[batch, :, :].detach().numpy()# + np.random.normal(0, self.epsilon, (n_agents, n_actions))
+
+                _, col_ind = scipy.optimize.linear_sum_assignment(benefit_matrix_from_q_values, maximize=True)
+                picked_actions[batch, :] = th.tensor(col_ind)
+            return picked_actions
+        else:
+            # mask actions that are excluded from selection
+            masked_q_values = agent_inputs.clone()
+            masked_q_values[avail_actions == 0.0] = -float("inf")  # should never be selected!
+
+            random_numbers = th.rand_like(agent_inputs[:, :, 0])
+            pick_random = (random_numbers < self.epsilon).long()
+            random_actions = Categorical(avail_actions.float()).sample().long()
+
+            picked_actions = pick_random * random_actions + (1 - pick_random) * masked_q_values.max(dim=2)[1]
+            return picked_actions
+
+
+REGISTRY["epsilon_greedy_sap_test"] = EpsilonGreedySAPTestActionSelector
 
 class SoftPoliciesSelector():
 
